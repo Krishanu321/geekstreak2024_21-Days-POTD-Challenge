dde895c8569fa17fc9b225d920205ab1e7168b1f
Krishanu Santra <krishanusantra345@gmail.com>
2024-09-15 23:55:08 +0530

    21-Days-POTD

---
+++ day5.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+public:
+    Node *head = NULL, *lastNode = NULL;
+    void help(Node *root)
+    {
+        if (!root)
+            return;
+        help(root->left);
+        if (!head)
+            head = root;
+        if (lastNode)
+            lastNode->right = root;
+        root->left = lastNode;
+        lastNode = root;
+        help(root->right);
+    }
+    Node *bToDLL(Node *root)
+    {
+        // code here
+        help(root);
+        return head;
+    }
+};
