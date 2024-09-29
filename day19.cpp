c198938eef85673ca2945e3ddf9b388972e6bf68
Krishanu Santra <krishanusantra345@gmail.com>
2024-09-29 22:13:06 +0530

    Total count

---
+++ day19.cpp
@@ -0,0 +1,14 @@
+class Solution
+{
+public:
+    int totalCount(int k, vector<int> &arr)
+    {
+        // code here
+        int ans = 0;
+        for (auto x : arr)
+        {
+            ans += (x + k - 1) / k;
+        }
+        return ans;
+    }
+};
